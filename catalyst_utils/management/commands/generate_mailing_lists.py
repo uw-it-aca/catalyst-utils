997befa6d5dbe6b2b15bf34787ebddccd9b508d3
Jim Laney <jlaney@uw.edu>
2021-11-15 14:26:04 -0800

    adds authz models

--- catalyst_utils/management/commands/generate_mailing_lists.py
+++ catalyst_utils/management/commands/generate_mailing_lists.py
@@ -4,7 +4,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files.storage import default_storage
 from catalyst_utils.models import Survey
+from logging import getLogger
 import csv
+
+logger = getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -17,35 +20,54 @@ class Command(BaseCommand):
         survey_owner_writer.writerow([
             'uwnetid', 'first_name', 'last_name', 'last_login_date'])
 
-        survey_admin_outfile = default_storage.open(
+        survey_netid_admin_outfile = default_storage.open(
             'survey_netid_admins.csv', 'w')
-        survey_admin_writer = csv.writer(
-            survey_admin_outfile, dialect='unix_newline')
-        survey_admin_writer.writerow([
+        survey_netid_admin_writer = csv.writer(
+            survey_netid_admin_outfile, dialect='unix_newline')
+        survey_netid_admin_writer.writerow([
             'uwnetid', 'first_name', 'last_name', 'last_login_date',
             'shared_netids'])
 
-        admins = {}
-        for person in Survey.objects.get_survey_owners():
-            if person.is_person:
-                if person.is_current:
-                    survey_owner_writer.writerow(person.csv_data())
+        owners = set()
+        administrators = set()
+        netid_admins = {}
+        for survey in Survey.objects.get_all_surveys(year=None):
+            owner = survey.owner
+            if owner.is_person:
+                if owner.is_current:
+                    owners.add(owner)
             else:
-                for admin in person.get_admins():
+                for admin in owner.get_admins():
                     if admin.is_person and admin.is_current:
-                        if admin.person_id in admins:
-                            admins[admin.person_id].get(
-                                'shared_netids', []).append(person.login_name)
+                        if admin.person_id in netid_admins:
+                            netid_admins[admin.person_id].get(
+                                'shared_netids', []).append(owner.login_name)
                         else:
-                            admins[admin.person_id] = {
+                            netid_admins[admin.person_id] = {
                                 'person': admin,
-                                'shared_netids': [person.login_name],
+                                'shared_netids': [owner.login_name],
                             }
 
-        for person_id in admins:
-            csv_data = admins[person_id]['person'].csv_data()
-            csv_data.append(','.join(admins[person_id]['shared_netids']))
-            survey_admin_writer.writerow(csv_data)
+            for administrator in survey.administrators:
+                if administrator.is_person:
+                    if administrator.is_current:
+                        administrators.add(administrator)
+                else:
+                    logger.info('Non-personal administrator: {}'.format(
+                        administrator.login_name))
+
+
+        for person in owners:
+            survey_owner_writer.writerow(person.csv_data())
+
+        for administrator in administrators:
+            pass
+
+        for person_id in netid_admins:
+            person = netid_admins[person_id]['person']
+            csv_data = person.csv_data()
+            csv_data.append('|'.join(netid_admins[person_id]['shared_netids']))
+            survey_netid_admin_writer.writerow(csv_data)
 
         survey_owner_outfile.close()
-        survey_admin_outfile.close()
+        survey_netid_admin_outfile.close()
