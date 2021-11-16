9ce3569ed8a6ce9a8ae16ff291a3d69577df2e4f
Jim Laney <jlaney@uw.edu>
2021-11-16 09:07:07 -0800

    drops id in migration

--- catalyst_utils/migrations/0002_groupwrapper_peopleincrowd_roleimplementation.py
+++ catalyst_utils/migrations/0002_groupwrapper_peopleincrowd_roleimplementation.py
@@ -25,7 +25,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PeopleInCrowd',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('crowd_id', models.IntegerField()),
             ],
             options={
