d0160a49c4aa7b510d99d39d454e6d2902e85742
Jim Laney <jlaney@uw.edu>
2021-11-30 11:05:30 -0800

    one more

--- catalyst_utils/tests/dao/test_group.py
+++ catalyst_utils/tests/dao/test_group.py
@@ -3,6 +3,7 @@
 
 from django.test import TestCase
 from uw_gws.utilities import fdao_gws_override
+from uw_gws.exceptions import InvalidGroupID
 from catalyst_utils.dao.group import get_group_members
 
 
@@ -13,3 +14,4 @@ class GroupDAOFunctionsTest(TestCase):
         self.assertEqual(len(get_group_members(group_id)), 2)
         self.assertEqual(len(get_group_members(group_id, effective=True)), 3)
         self.assertEqual(len(get_group_members('u_acadev_doesnotexist')), 0)
+        self.assertRaises(InvalidGroupID, get_group_members, '')
