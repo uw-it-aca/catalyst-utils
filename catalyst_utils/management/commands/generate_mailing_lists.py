54232995a810fa06de4de7584e56c8b09ea8ae75
Jim Laney <jlaney@uw.edu>
2022-01-03 13:35:59 -0800

    fix license

--- catalyst_utils/management/commands/generate_mailing_lists.py
+++ catalyst_utils/management/commands/generate_mailing_lists.py
@@ -1,9 +1,6 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-# Copyright 2021 UW-IT, University of Washington
-# SPDX-License-Identifier: Apache-2.0
-
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files.storage import default_storage
 from catalyst_utils.models import Survey, Gradebook
