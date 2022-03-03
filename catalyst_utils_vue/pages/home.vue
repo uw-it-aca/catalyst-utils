38a41423f62f5ab7499fa76ca200470773068660
Charlon Palacay <charlon@gmail.com>
2022-03-02 17:32:27 -0800

    Check if array is empty.

--- catalyst_utils_vue/pages/home.vue
+++ catalyst_utils_vue/pages/home.vue
@@ -5,88 +5,175 @@
     <template #content>
       <div class="row">
         <div class="col">
+
+          <div class="mb-3">{{ surveyData }}</div>
+
           <h2>Your Surveys</h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos inventore doloremque
-            perspiciatis eius enim dicta ad quae? Blanditiis, quaerat voluptas, assumenda
-            necessitatibus, quisquam illum eos omnis inventore tempora corporis esse?
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione deserunt sapiente,
-            ipsam minima praesentium, eaque non animi nemo accusantium quia minus officiis dolorum
-            dolores. Quis possimus odit incidunt quaerat officia?
-          </p>
+          <p>{{ surveyData.owned_surveys }}</p>
+          <p>{{ typeof surveyData.owned_surveys }}</p>
 
           <div class="card mb-5">
-            <div class="card-body">
-              <table class="table mb-0">
+            <div class="card-body table-responsive-md">
+              <table v-if="surveyData.owned_surveys" class="table mb-0">
                 <thead>
                   <tr>
                     <th scope="col">Created</th>
                     <th scope="col">Name</th>
-                    <th scope="col">Owner</th>
                     <th scope="col">Info</th>
                     <th scope="col">&nbsp;</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="(ownedSurvey, index) in surveyDataList.owned_surveys">
-                    <td>{{ formatDate(ownedSurvey.created_date) }}</td>
+                  <tr v-for="(owned, index) in surveyData.owned_surveys" :key="index">
                     <td>
-                      <span v-if="ownedSurvey.name == null" class="text-muted">survey name null</span>
-                      <span v-else>{{ ownedSurvey.name }}</span>
+                      <div>{{ formatDate(owned.created_date) }}</div>
+                      <div class="small">{{ owned.owner.login_name }}</div>
                     </td>
-                    <td>{{ ownedSurvey.owner.name }} ({{ ownedSurvey.owner.login_name }})</td>
                     <td>
-                      Questions: <span v-if="ownedSurvey.question_count == null">0</span>
-                      <span v-else>{{ ownedSurvey.question_count }}</span>
+                      <span v-if="owned.name == null" class="text-muted"
+                        >null</span
+                      >
+                      <span v-else>{{ owned.name }}</span>
+                    </td>
+                    <td>
+                      <div>
+                        Questions:
+                        <span v-if="owned.question_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ owned.question_count }}</span>
 
-                      Responses: <span v-if="ownedSurvey.response_count == null">0</span>
-                      <span v-else>{{ ownedSurvey.response_count }}</span>
+                        Responses:
+                        <span v-if="owned.response_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ owned.response_count }}</span>
+                      </div>
+                      <div>
+                        Anonymous: {{ owned.is_research_anonymous }} Confidential:
+                        {{ owned.is_research_confidential }}
+                      </div>
                     </td>
-                    <td><button type="button" class="btn btn-primary btn-sm">Download</button></td>
+                    <td><button type="button" class="btn btn-dark-beige btn-sm">Download</button></td>
                   </tr>
                 </tbody>
               </table>
+              <div v-else>No data</div>
             </div>
           </div>
 
           <h2>Surveys Owned by Others Netids</h2>
-          <ul class="list-group mb-5">
-            <li class="list-group-item">
-              An item
-              <button type="button" class="btn btn-primary btn-sm">Download</button>
-            </li>
-            <li class="list-group-item">
-              A second item
-              <button type="button" class="btn btn-primary btn-sm">Download</button>
-            </li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">
-              And a fifth one | Questions, Responses, Code Translation, Rollup
-            </li>
-          </ul>
 
-          <h2>Surveys owned by shared netids you have Admin access to</h2>
-          <ul class="list-group mb-5">
-            <li class="list-group-item">
-              An item
-              <button type="button" class="btn btn-primary btn-sm">Download</button>
-            </li>
-            <li class="list-group-item">
-              A second item
-              <button type="button" class="btn btn-primary btn-sm">Download</button>
-            </li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">
-              And a fifth one | Questions, Responses, Code Translation, Rollup
-            </li>
-          </ul>
+          <p>{{ surveyData.netid_surveys }}</p>
+          <p>{{ typeof surveyData.netid_surveys }}</p>
 
-          <div class="my-5">{{ surveyDataList }}</div>
+          <div class="card mb-5">
+            <div class="card-body table-responsive-md">
+
+              <table v-if="!surveyData.netid_surveys == '[]'" class="table mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Created</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Info</th>
+                    <th scope="col">&nbsp;</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(netid, index) in surveyData.netid_surveys" :key="index">
+                    <td>
+                      <div>{{ formatDate(netid.created_date) }}</div>
+                      <div class="small">{{ netid.owner.login_name }}</div>
+                    </td>
+                    <td>
+                      <span v-if="netid.name == null" class="text-muted"
+                        >null</span
+                      >
+                      <span v-else>{{ netid.name }}</span>
+                    </td>
+                    <td>
+                      <div>
+                        Questions:
+                        <span v-if="netid.question_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ netid.question_count }}</span>
+
+                        Responses:
+                        <span v-if="netid.response_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ netid.response_count }}</span>
+                      </div>
+                      <div>
+                        Anonymous: {{ netid.is_research_anonymous }} Confidential:
+                        {{ netid.is_research_confidential }}
+                      </div>
+                    </td>
+                    <td><button type="button" class="btn btn-darl-beige btn-sm">Download</button></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div v-else>No data</div>
+            </div>
+          </div>
+
+          <h2>Surveys you have Admin access to</h2>
+
+          <p>{{ surveyData.admin_surveys }}</p>
+          <p>{{ typeof surveyData.admin_surveys }}</p>
+
+          <div class="card mb-5">
+            <div class="card-body table-responsive-md">
+              <table v-if="!surveyData.admin_surveys == '[]'" class="table mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Created</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Info</th>
+                    <th scope="col">&nbsp;</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(admin, index) in surveyData.admin_surveys" :key="index">
+                    <td>
+                      <div>{{ formatDate(admin.created_date) }}</div>
+                      <div class="small">{{ admin.owner.login_name }}</div>
+                    </td>
+                    <td>
+                      <span v-if="admin.name == null" class="text-muted"
+                        >null</span
+                      >
+                      <span v-else>{{ admin.name }}</span>
+                    </td>
+                    <td>
+                      <div>
+                        Questions:
+                        <span v-if="admin.question_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ admin.question_count }}</span>
+
+                        Responses:
+                        <span v-if="admin.response_count == null" class="text-muted"
+                          >null</span
+                        >
+                        <span v-else>{{ admin.response_count }}</span>
+                      </div>
+                      <div>
+                        Anonymous: {{ admin.is_research_anonymous }} Confidential:
+                        {{ admin.is_research_confidential }}
+                      </div>
+                    </td>
+                    <td><button type="button" class="btn btn-dark-beige btn-sm">Download</button></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div v-else>No data</div>
+            </div>
+          </div>
+
         </div>
       </div>
     </template>
@@ -95,7 +182,7 @@
 
 <script>
 import Layout from '../layout.vue';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 export default {
   components: {
@@ -104,24 +191,27 @@ export default {
   data() {
     return {
       pageTitle: 'Surveys',
-      surveyDataList: [],
+      surveyData: [],
     };
   },
   methods: {
     getSurveyData() {
       fetch('/api/v1/survey')
         .then((response) => response.json())
-        .then((data) => (this.surveyDataList = data));
+        .then((data) => {
+          this.surveyData = data;
+        });
     },
     formatDate(dateString) {
       const date = dayjs(dateString);
       // Then specify how you want your dates to be formatted
-      return date.format("MMMM D, YYYY");
-    },
+      return date.format('MMMM D, YYYY');
+    }
   },
-  mounted() {
+  created() {
+    // fetch the survey data
     this.getSurveyData();
-  },
+  }
 };
 </script>
 
