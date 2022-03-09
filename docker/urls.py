2c1e6dcc1f21fc18be5233f3a72ae2fe267e6220
Jim Laney <jlaney@uw.edu>
2022-03-09 11:43:23 -0800

    adjust project urls again

--- docker/urls.py
+++ docker/urls.py
@@ -2,8 +2,8 @@ from .base_urls import *
 from django.urls import include, re_path
 
 urlpatterns += [
-    re_path(r'^support/', include('userservice.urls')),
-    re_path(r'^restclients/', include('rc_django.urls')),
-    re_path(r'^persistent_message/', include('persistent_message.urls')),
+    re_path(r'^support', include('userservice.urls')),
+    re_path(r'^restclients', include('rc_django.urls')),
+    re_path(r'^persistent_message', include('persistent_message.urls')),
     re_path(r'^', include('catalyst_utils.urls')),
 ]
